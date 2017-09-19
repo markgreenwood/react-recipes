d03d768e7e96b3dd8570da1710be6ae3e0801444
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-09-19 09:30:55 -0700

    Initial code

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = ({ recipes }) =>
+  <article>
+    <header>
+      <h1>Delicious Recipes</h1>
+    </header>
+  </article>;
+
+Menu.displayName = 'Menu';
+
+export default Menu;
